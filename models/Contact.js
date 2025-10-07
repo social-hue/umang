7028cbac44de8ad94bf678cce2b64e9858a1e16d
Aman Rawat <amanrawat.js@gmail.com>
2025-10-07 00:05:37 -0700

    fixing Contact Schema

--- models/Contact.js
+++ models/Contact.js
@@ -5,7 +5,7 @@ const ContactSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true },
-    number: { type: Integer, required:true },
+    number: { type: Number, required:true },
     message: { type: String, required: true }
   },
   { timestamps: true }
