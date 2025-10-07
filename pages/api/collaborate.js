6ac1e508a20c8dfa2c61804e4a7969e6ea74e414
Aman Rawat <amanrawat.js@gmail.com>
2025-10-07 00:02:16 -0700

    fixing projects section

--- pages/api/collaborate.js
+++ pages/api/collaborate.js
@@ -1,9 +1,9 @@
-import { ColabController } from "@/controllers/colabdata";
+// import { ColabController } from "@/controllers/colabdata";
 
-export default async function handler(req, res) {
-  if (req.method === "POST") {
-    await ColabController(req, res);
-  } else {
-    res.status(405).json({ error: "Method Not Allowed" });
-  }
-}
+// export default async function handler(req, res) {
+//   if (req.method === "POST") {
+//     await ColabController(req, res);
+//   } else {
+//     res.status(405).json({ error: "Method Not Allowed" });
+//   }
+// }
