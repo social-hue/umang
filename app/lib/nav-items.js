73b1f9d326fc095cc21917f2f8e851e5d9c1a86d
Aman Rawat <amanrawat.js@gmail.com>
2025-11-24 10:28:08 +0530

    fixing the footer elements

--- app/lib/nav-items.js
+++ app/lib/nav-items.js
@@ -4,7 +4,7 @@ export const navItems = [
   { label: "Services", href: "" },
   { label: "Projects", href: "/projects" },
   // { label: "Life", href: "/lifeAt" },
-  { label: "Franchise Program", href: "/partners" },
+  { label: "Franchise", href: "/partners" },
   { label: "Blog", href: "/blog" },
   // { label: "Media Gallery", href: "/mediaGallery" },
   { label: "Contact", href: "/contact" },
